428cda49eaaaff53283ebdf5d246a2b885e73a8e
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-24 17:13:36 +0530

    create user schema

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("users", UserSchema);
