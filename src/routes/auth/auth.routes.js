36b95d411fe2163f848251e644ac884af4937733
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-04-25 09:10:45 +0530

    add to databse

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -3,6 +3,11 @@ import { Router } from "express";
 import { check, validationResult } from "express-validator";
 import authBodyValidator from "../../middlewares/auth/authBodyValidator";
 import bcrypt from "bcryptjs";
+import { model } from "mongoose";
+
+const router = Router();
+
+const User = model("users");
 
 const authValidator = [
   check("email", "Enter the valid email address").isEmail(),
@@ -11,19 +16,25 @@ const authValidator = [
   }),
 ];
 
-const router = Router();
-
 //Login
 router.post("/login", authValidator, authBodyValidator, async (req, res) => {});
 
 //Register
 router.post("/register", authValidator, authBodyValidator, async (req, res) => {
-  let { email, password } = req.body;
+  try {
+    let { email, password } = req.body;
 
-  const salt = await bcrypt.genSalt(10);
-  password = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
 
-  return res.status(200).json({ email, password });
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    return res.status(200).json(newUser);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ errors: [{ msg: "internal server error" }] });
+  }
 });
 
 export default router;
